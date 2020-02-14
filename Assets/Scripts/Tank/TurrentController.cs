590f1bf74b2fb535a0611281da93d8a89f91b3af
DaniPK7 <51991417+DaniPK7@users.noreply.github.com>
2020-02-14 16:57:11 +0100

    intetnto ejer 3

---
+++ Assets/Scripts/Tank/TurrentController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurrentController : MonoBehaviour
+{
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion. Euler(Input.mousePosition.x, 0f, Input.mousePosition.z);
+        //arreglar
+    }
+}
